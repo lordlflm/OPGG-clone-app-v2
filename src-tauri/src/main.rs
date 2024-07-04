aab44b715e9bdeaaccddf5a3afa265205f31d4ed
Thomas Laflamme <bc991967@ens.uqam.ca>
2024-07-04 10:03:28 -0400

    begin riot api call

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,9 @@
 
 use dotenv::dotenv;
 use lazy_static::lazy_static;
+use urlencoding::encode;
+use reqwest::Error;
+use serde::Deserialize;
 
 lazy_static! {
   static ref RIOT_API_KEY: String = {
@@ -20,7 +23,7 @@ fn main() {
 }
 
 #[tauri::command]
-fn get_account(data: std::collections::HashMap<String, String>) -> Result<String, String> {
+async fn get_account(data: std::collections::HashMap<String, String>) -> Result<std::collections::HashMap<String, String>, String> {
   let summoner_name: String;
   let summoner_tag: String;
   let summoner_region: String;
@@ -39,15 +42,33 @@ fn get_account(data: std::collections::HashMap<String, String>) -> Result<String
   }
 
   match data.get("summoner-region") {
-    Some(region) => summoner_region = region,
+    Some(region) => summoner_region = region.to_string(),
     None => return Err("Something went wrong: No summoner region".to_string()),
   }
+
+  let puuid_response = reqwest::get(format!("https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key={api_key}",
+                                          region = "americas",
+                                          name = encode(&summoner_name),
+                                          tag = summoner_tag,
+                                          api_key = *RIOT_API_KEY))
+    .await?;
+
+  // #[derive(Deserialize, Debug)]
+  // struct Puuid {
+  //   puuid: std::collections::HashMap<String, String>,
+  //   name: std::collections::HashMap<String, String>,
+  //   tag: std::collections::HashMap<String, String>,
+  // }
+
+  // let pid: Vec<Puuid> = puuid_response.json();
+  // println!("{:?}", pid);
+
 
   // 1. make riot api call
   // 2. if we get a summoner object render html page
   // 3. if not stay on page and render error msg
 
-  // println!("{}", *RIOT_API_KEY);
-
-  Ok("Summoner found".to_string())
+  let mut response = std::collections::HashMap::new();
+  response.insert("success".to_string(), "true".to_string());
+  Ok(response)
 }
