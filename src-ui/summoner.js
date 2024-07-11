04be1f508d1ad87b127845c09e61ebc40b9ee52b
lordlflm <thomdlaf@gmail.com>
2024-07-11 10:51:51 -0400

    add highest tier ranked icon

--- src-ui/summoner.js
+++ src-ui/summoner.js
@@ -12,6 +12,7 @@ class Summoner {
         this.server = null;
         this.soloLeague = new League;
         this.flexLeague = new League;
+        this.highestTier = null;
     }
 }
 
@@ -25,6 +26,19 @@ class League {
         this.losses = null;
     }
 }
+
+const tierValues = new Map([
+    ['IRONS', 0],
+    ['BRONZE', 1],
+    ['SILVER', 2],
+    ['GOLD', 3],
+    ['PLATINUM', 4],
+    ['EMERALD', 5],
+    ['DIAMOND', 6],
+    ['MASTER', 7],
+    ['GRANDMASTER', 8],
+    ['CHALLENGER', 9]
+]);
 
 let summoner = new Summoner();
 
@@ -57,9 +71,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 document.getElementById("rank-type-select").addEventListener("change", () => {
     rankType = document.getElementById("rank-type-select").value;
-    console.debug(rankType);
     if (rankType === "flex") {
-        console.debug("OKKKK FLEXXX");
         displayFlexRank();
     } else {
         displaySoloRank();
@@ -89,6 +101,9 @@ async function fetch_leagues() {
                 summoner.soloLeague.leaguePoints = league.leaguePoints;
                 summoner.soloLeague.wins = league.wins;
                 summoner.soloLeague.losses = league.losses;
+                if (summoner.highestTier == null || tierValues.get(summoner.soloLeague.tier) > tierValues.get(summoner.highestTier)) {
+                    summoner.highestTier = summoner.soloLeague.tier;
+                }
             }
             if (league.queueType && league.queueType.slice(1, -1) === "RANKED_FLEX_SR") {
                 summoner.flexLeague.leagueType = league.queueType.slice(1, -1);
@@ -97,6 +112,9 @@ async function fetch_leagues() {
                 summoner.flexLeague.leaguePoints = league.leaguePoints;
                 summoner.flexLeague.wins = league.wins;
                 summoner.flexLeague.losses = league.losses;
+                if (summoner.highestTier == null || tierValues.get(summoner.flexLeague.tier) > tierValues.get(summoner.highestTier)) {
+                    summoner.highestTier = summoner.flexLeague.tier;
+                }
             }
         }
     } else {
@@ -110,6 +128,10 @@ async function displaySummoner() {
     document.getElementById("server").textContent = summoner.server;
     document.getElementById("level").textContent = summoner.level;
     document.getElementById("icon").src = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${summoner.iconId}.jpg`;
+    if (summoner.highestTier) {
+        // TODO this icon can be better
+        document.getElementById("rank-icon").src = `https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-static-assets/global/default/images/ranked-emblem/emblem-${summoner.highestTier.toLowerCase()}.png`;
+    }
     displaySoloRank();
 }
 
